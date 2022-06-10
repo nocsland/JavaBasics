cd7f23d5f69c281d1593f74285009832b2d273a9
nocsland <nocsland@gmail.com>
2022-06-10 08:30:53 +0400

    added first exercises

---
+++ JavaBasics/src/main/java/Exercises/Ex1_1.java
@@ -0,0 +1,9 @@
+package Exercises;
+
+// По традиции любой язык программирования начинают изучать с первой программы, которая выводит на экран надпись
+// «Привет, мир!».
+public class Ex1_1 {
+    public static void main(String[] args) {
+        System.out.println("Привет, мир!");
+    }
+}
